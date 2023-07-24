6d8bc533c7f9a97b0895387f94b8c4a73e522766
HAMDAAN AHMAD <85315584+githamdaan25@users.noreply.github.com>
2023-07-25 02:08:02 +0530

    Create string.cpp

---
+++ Find the Index of the First Occurrence in a String/string.cpp
@@ -0,0 +1,14 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        if not needle:
+            return 0
+
+        needle_length = len(needle)
+        
+        haystack_length = len(haystack)
+
+        for i in range(haystack_length - needle_length + 1):
+            if haystack[i:i + needle_length] == needle:
+                return i
+
+        return -1
